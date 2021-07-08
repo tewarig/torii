bd2a8121ac56520744d6023d392807cca07d45b0
Gaurav Tewari <gauravtewari111@gmail.com>
2021-07-08 18:53:29 +0530

    chakra ui added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ChakraProvider} from "@chakra-ui/react"
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
